a122be20ad7161ef22aa790af6927b713141bef6
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-26 06:21:18 +0100

    test: add shared Laravel test case

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    // Shared base test case for Lamii feature and unit tests.
+}
