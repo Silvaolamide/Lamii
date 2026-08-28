7119923d65717412d13c919d819f81abc9773dde
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-28 12:34:26 +0100

    Remove Apple and X OAuth configuration

--- config/services.php
+++ config/services.php
@@ -6,14 +6,4 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
-    'apple' => [
-        'client_id' => env('APPLE_CLIENT_ID'),
-        'client_secret' => env('APPLE_CLIENT_SECRET'),
-        'redirect' => env('APPLE_REDIRECT_URI'),
-    ],
-    'x' => [
-        'client_id' => env('X_CLIENT_ID'),
-        'client_secret' => env('X_CLIENT_SECRET'),
-        'redirect' => env('X_REDIRECT_URI'),
-    ],
 ];
