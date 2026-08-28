d968efb2f6b304bfa2df8a11022c5a60d21807ab
Olamide Agunkejoye <Silvaolamide@gmail.com>
2026-08-28 11:28:38 +0100

    Register onboarding middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureOnboardingCompleted;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Application middleware.
+        $middleware->alias([
+            'onboarding' => EnsureOnboardingCompleted::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // Exception configuration.
